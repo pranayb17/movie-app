80d2b3ad32c3d7a7ef93a7eba53de3824f1ba3eb
unknown <pranayborkar133@gmail.com>
2021-11-25 01:51:21 +0530

    Add combine combineReducers()

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,20 @@
-import { ADD_MOVIES, ADD_TO_FAVOURITE, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES } from '../actions'
+import { combineReducers } from 'redux';
+
+import {
+    ADD_MOVIES,
+    ADD_TO_FAVOURITE,
+    REMOVE_FROM_FAVOURITES,
+    SET_SHOW_FAVOURITES
+} from '../actions'
 
 const initialMoviesState = {
     list: [],
     favourites: [],
     showFavourites: false
-}
+};
 
-export default function movies(state = initialMoviesState, action) {
+export function movies(state = initialMoviesState, action) {
+    console.log('MOVIES REDUCER');
     // if (action.type === ADD_MOVIES) {
     //     return {
     //         ...state , //spread the state
@@ -44,5 +52,33 @@ export default function movies(state = initialMoviesState, action) {
             return state;
     }
 }
+
+const initialSearchState = {
+    result: {}
+};
+
+export function search(state = initialSearchState, action) {
+    console.log('SEARCH REDUCER');
+    return state;
+}
+
+const initialRootState = {
+    movies: initialMoviesState,
+    search: initialSearchState
+}
+
+// export default function rootReducer(state = initialRootState, action) {
+//     return {
+//         movies: movies(state.movies, action),
+//         search: search(state.search, action)
+//     }
+// }
+
+export default combineReducers({
+    movies,
+    search
+});
+
+
 
 
