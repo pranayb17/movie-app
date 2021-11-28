a0056b1931aa83414668a8b7867101a603706d0d
unknown <pranayborkar133@gmail.com>
2021-11-28 20:57:28 +0530

    Navbar updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,35 +10,48 @@
 //action types
 export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE';
-export const REMOVE_FROM_FAVOURITES =  'REMOVE_FROM_FAVOURITES';
-export const SET_SHOW_FAVOURITES =  'SET_SHOW_FAVOURITES';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
+export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
 
 //action creators
-export function addMovies (movies) {
+export function addMovies(movies) {
     return {
         type: ADD_MOVIES,
-        movies  
-    }
+        movies
+    };
 }
 
-export function addFavourite (movie) {
+export function addFavourite(movie) {
     return {
-       type: ADD_TO_FAVOURITE,
-        movie  
-    }
+        type: ADD_TO_FAVOURITE,
+        movie
+    };
 }
 
-export function removeFromFavourites (movie) {
+export function removeFromFavourites(movie) {
     return {
-       type: REMOVE_FROM_FAVOURITES,
-        movie  
-    }
+        type: REMOVE_FROM_FAVOURITES,
+        movie
+    };
 }
 
-export function setShowFavourites (val) {
+export function setShowFavourites(val) {
     return {
-       type: SET_SHOW_FAVOURITES,
-       val  
-    }
+        type: SET_SHOW_FAVOURITES,
+        val
+    };
+}
+
+export function addMovieToList(movie) {
+    return {
+        type: ADD_MOVIE_TO_LIST,
+        movie
+    };
+}
+
+
+export function handleMovieSearch(movie) {
+
 }
 
